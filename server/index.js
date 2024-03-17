257ddd4b1a2250a54cf43295df4526e7af26fc7e
PoorGenius <albinhasanaj06@hotmail.com>
2024-03-17 14:43:19 +0100

    First Commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import dalleRoutes from "./routes/dalle.routes.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/dalle", dalleRoutes)
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello from DALL-E" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
